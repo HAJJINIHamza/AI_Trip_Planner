5ac8e9fa711a6695b0d074c24e2584a3d4dbb2b2
HAJJINIHamza <hajjinihamza2@gmail.com>
2025-11-19 00:20:45 +0100

    TEST agent workflow, api end point and ui

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -21,7 +21,7 @@ class GraphBuilder():
 
         self.tools.extend(
             [* self.weather_tools.weather_tool_list,
-             * self.place_search_tool.place_search_tool_list,
+             * self.place_search_tools.place_search_tool_list,
              * self.calculator_tools.calculator_tool_list,
              * self.currency_converter_tools.currency_converter_tool_list
              ]
