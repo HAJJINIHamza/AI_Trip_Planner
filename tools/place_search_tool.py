9f8d27573704e772f53b0f5e276f6890f59b3f6f
HAJJINIHamza <hajjinihamza2@gmail.com>
2025-11-22 13:14:19 +0100

    ADD place search tool

--- tools/place_search_tool.py
+++ tools/place_search_tool.py
@@ -21,14 +21,15 @@ class PlaceSearchTool:
             """
             Search attractions of a place
             """
+            print ("@TOOL : search_attractions is being used")
             try:
                 attraction_result = self.google_places_search.google_search_attractions(place)
                 if attraction_result:
-                    print ("@TOOL : search_attractions is being used")
                     print (f"Following are the attractions of {place} as suggested by google: {attraction_result}")
                     return f"Following are the attractions of {place} as suggested by google: {attraction_result}"
             except Exception as e:
                 tavily_result = self.tavily_search.tavily_search_attractions(place)
+                print (f"Following are the attractions of {place} as suggested by tavily: {tavily_result}")
                 return f"Google cannot find the details due to {e}. \nFollowing are the attractions of {place}: {tavily_result}"
             
         @tool 
@@ -36,14 +37,16 @@ class PlaceSearchTool:
             """
             Search restaurants of a place 
             """
+            print ("@TOOL : search_restaurants is being used")
             try:
                 result = self.google_places_search.google_search_restaurants(place)
                 if result:
-                    print ("@TOOL : search_restaurants is being used")
+                   
                     print(f"Following are the restaurants of {place} as suggested by google: {result}")
                     return f"Following are the restaurants of {place} as suggested by google: {result}"
             except Exception as e:
                 tavily_result = self.tavily_search.tavily_search_restaurants (place)
+                print(f"Google cannot find the details due to {e}. \nFollowing are the restaurants of {place}: {tavily_result}")
                 return f"Google cannot find the details due to {e}. \nFollowing are the restaurants of {place}: {tavily_result}"
             
         @tool
@@ -51,6 +54,7 @@ class PlaceSearchTool:
             """
             Search activities of a place
             """
+            print ("@TOOL : search_activities is being used")
             try:
                 result = self.google_places_search.google_search_activity (place)
                 if result:
@@ -59,6 +63,7 @@ class PlaceSearchTool:
                     return f"Following are the activities of {place} as suggested by google: {result}"
             except Exception as e:
                 tavily_result = self.tavily_search.tavily_search_activity (place)
+                print (f"Google cannot find the details due to {e}. \nFollowing are the activities of {place}: {tavily_result}")
                 return f"Google cannot find the details due to {e}. \nFollowing are the activities of {place}: {tavily_result}"
             
         @tool
@@ -66,6 +71,7 @@ class PlaceSearchTool:
             """
             Search transportation of a place
             """
+            print ("@TOOL : search_transportation is being used")
             try:
                 result = self.google_places_search.google_search_transportation (place)
                 if result:
@@ -74,6 +80,7 @@ class PlaceSearchTool:
                     return f"Following are modes of transportation available in {place} as suggested by google: {result}"
             except Exception as e:
                 tavily_result = self.tavily_search.tavily_search_transportation (place)
+                print (f"Google cannot find the details due to {e}. \nFollowing are the transportation of {place}: {tavily_result}")
                 return f"Google cannot find the details due to {e}. \nFollowing are the transportation of {place}: {tavily_result}"
             
         return [search_attractions, search_restaurants, search_activities, search_transportation] 
